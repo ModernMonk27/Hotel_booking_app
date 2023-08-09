2338459b0f280a47115ca224866303fd9e826470
monk <lakshmiaravind.atom@gmail.com>
2023-08-09 11:16:50 +0530

    created hotel and reservation class

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pandas as pd
 
 from fpdf import FPDF
 
-df = pd.read_csv("hotels.csv", dtype= str)
+df = pd.read_csv("hotels.csv", dtype=str)
+
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
+df_security = pd.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
     def __init__(self, hotelx):
         self.physc = hotelx
-        self.ole = df.loc[df["id"] == self.physc , "name"].squeeze()
+        self.ole = df.loc[df["id"] == self.physc, "name"].squeeze()
 
     def book(self):
         """ this method changes the availability of hotel in csv file to no"""
@@ -47,7 +51,6 @@ class Registration:
         return content
 
     def pdfgen(self):
-
         pdf = FPDF(orientation="P", format="A4", unit="mm")
         pdf.add_page()
         content = f"""
@@ -82,6 +85,33 @@ class Registration:
         pdf.set_font(family="Times", size=16, style="B")
         pdf.cell(w=80, h=6, txt=f"Have a great stay ", ln=1)
         pdf.output("ticket.pdf")
+        return content
+
+
+class Credit_card:
+    def __init__(self, number):
+        self.physc = number
+
+    def validate(self, expire, cvv, holder):
+        credit = {"number": self.physc, "expiration": expire, "cvc": cvv, "holder": holder}
+
+        if credit in df_cards:
+            return True
+        else:
+            return False
+
+class SecureCard(Credit_card):
+
+    def authenticate(self,ps):
+
+        password = df_security.loc[df_security["number"] == self.physc, "password"].squeeze()
+
+        if password == ps:
+            return True
+        else:
+            return False
+
+
 
 
 
@@ -93,10 +123,20 @@ hotel_ID = input("Enter the hotel ID : ")
 hotel = Hotel(hotelx=hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    customer_details = input("Enter your name : ")
-    reserve = Registration(customer_details, hotel)
-    reserve.pdfgen()
+    credit_card = SecureCard(number="1234")
+    if credit_card.validate(expire="12/26", cvv="123", holder="JOHN SMITH"):
+        if credit_card.authenticate(ps="mypass"):
+            hotel.book()
+            customer_details = input("Enter your name : ")
+            reserve = Registration(customer_details, hotel)
+            reserve.pdfgen()
+        else:
+            print("authentication is failed..")
+
+
+    else:
+        print("credit card is wrong")
+
 
 else:
     print("hotel is not free..!")
